29ecd72d496403110fbf652cc0872f04ab965c21
hodisanmihai <mihaihodisn@gmail.com>
2025-05-07 01:02:53 +0300

    update

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -4,6 +4,7 @@ import { createClient } from "@sanity/client";
 const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!, // ID-ul proiectului tău din Sanity
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!, // Numele dataset-ului pe care îl folosești (de obicei, `production`)
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   useCdn: true, // Folosește CDN pentru a accelera citirea datelor
 });
 
